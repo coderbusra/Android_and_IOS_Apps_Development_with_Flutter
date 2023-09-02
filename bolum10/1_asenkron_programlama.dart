12560da0dde14c3ee909dded7c3c19460fdd5517
busradertli <busradertli000@gmail.com>
2023-09-02 09:35:56 +0300

    Update 1_asenkron_programlama.dart

--- bolum10/1_asenkron_programlama.dart
+++ bolum10/1_asenkron_programlama.dart
@@ -10,7 +10,7 @@ void main(List<String> args) {
   sonuc.then((String value) => print(value))
   .catchError((hata) {
     print(hata);
-  });
+  }).whenComplete(() => print("Ekmek alma operasyonu bitti"));
 
   print(sonuc);
   print("Peynir zeytin hazırlanır");
@@ -24,9 +24,9 @@ void main(List<String> args) {
 
 Future<String> uzunSurenIslem() {  
   
-  return Future.delayed(Duration(seconds: 2), () {
-    //return "Çocuk ekmekle eve girer";
-    throw Exception("Bakkalda ekmem kalmamış");
+  Future<String> sonuc = Future.delayed(Duration(seconds: 2), () {
+    return "Çocuk ekmekle eve girer";
+    //throw Exception("Bakkalda ekmek kalmamış");
   });
 
   return sonuc;
